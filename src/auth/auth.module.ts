a3daba1b67a92fc720cb5a833446a1b6641ae762
Maria Villen <9887543+MariaVillen@users.noreply.github.com>
2024-03-03 13:17:06 +0100

    modules missing fix

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,11 +3,12 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UsersService } from 'src/users/users.service';
 import { UsersModule } from 'src/users/users.module';
+import { FilesService } from 'src/files/files.service';
 
 @Global()
 @Module({
   imports: [UsersModule],
   controllers: [AuthController],
-  providers: [AuthService, UsersService],
+  providers: [AuthService, UsersService, FilesService],
 })
 export class AuthModule {}
--- src/cron/cron.module.ts
+++ src/cron/cron.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { CronService } from 'src/cron/cron.service';
+import { FilesService } from 'src/files/files.service';
 import { MailerModule } from 'src/mailer/mailer.module';
 import { MailerService } from 'src/mailer/mailer.service';
 import { OAuthConfig } from 'src/mailer/oauth.config';
@@ -8,6 +9,12 @@ import { UsersService } from 'src/users/users.service';
 
 @Module({
   imports: [MailerModule, UsersModule],
-  providers: [CronService, MailerService, UsersService, OAuthConfig],
+  providers: [
+    CronService,
+    MailerService,
+    UsersService,
+    OAuthConfig,
+    FilesService,
+  ],
 })
 export class CronModule {}
